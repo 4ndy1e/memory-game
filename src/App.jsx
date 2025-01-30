f9b58d93c83dea6555d953ff66f3ba3310884b3d
Andy Le <4ndyle@gmail.com>
2025-01-30 00:33:16 -0700

    use api to fetch random pokemon for game

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    // fetch returns a response object
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+      .then((result) => {
+        // then convert result into json
+        return result.json();
+      })
+      .then((data) => {
+        // log information
+        console.log(data);
+        console.log(data.results);
+        setPokemons(data.results);
+      });
+  }, []);
+
   return (
     <div>
-      <p> Hello World </p>
+      <div className="p-3">
+        <h1> PokéMatch </h1>
+        {pokemons.map((pokemons) => (
+          <p key={pokemons.name}> {pokemons.name} </p>
+        ))}
+      </div>
     </div>
   );
 }
