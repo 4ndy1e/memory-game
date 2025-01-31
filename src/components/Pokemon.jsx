6d0a50172f6cea37be9959e91e500e0339ced784
Andy Le <4ndyle@gmail.com>
2025-01-31 00:16:00 -0700

    update api fetching to prevent memory leaks

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -5,25 +5,38 @@ function Pokemon({ name }) {
   const [pokemon, setPokemons] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     // fetch returns a response object
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, { signal })
       .then((result) => {
         // then convert result into json
         return result.json();
       })
       .then((data) => {
-        // log information
         console.log(data);
-        // console.log(data.sprites.front_default);
-        // console.log(data.forms[0].name);
         setPokemons(data);
+      })
+      .catch((error) => {
+        console.log(error);
       });
+
+    // abort the request whenever the name changes or unmount
+    return () => {
+      controller.abort();
+    };
   }, [name]);
 
   return (
     <div>
       {pokemon && (
-        <div className="p-3 border-2 rounded-2xl flex flex-col w-fit hover:bg-red-300 hover:cursor-pointer m-4">
+        <div
+          className="p-3 border-2 rounded-2xl flex flex-col w-fit hover:bg-red-300 hover:cursor-pointer m-4"
+          onClick={() => {
+            console.log({ name });
+          }}
+        >
           <img
             src={pokemon.sprites.front_default}
             alt={pokemon.forms[0].name}
